aebe50caff96f5d61e4db251830fbbdb91473898
Lukas Holota <l.holota@outlook.com>
2020-02-14 13:36:51 +0100

    Fix

--- MyDockerNetworkPlugin_AllocateNetwork.go
+++ MyDockerNetworkPlugin_AllocateNetwork.go
@@ -12,7 +12,7 @@ func (self *MyDockerNetworkPlugin) AllocateNetwork(req *n.AllocateNetworkRequest
 	reqJson, _ := json.Marshal(req)
 	log.Printf("Received AllocateNetwork req:\n%+v\n", string(reqJson))
 
-	response, err := mac.AllocateNetwork(req)
+	response, err := mac.NetworkAllocate(req.NetworkID, req.Options, req.IPv4Data, req.IPv6Data)
 
 	respJson, _ = json.Marshal(response)
 	log.Printf("Returning AllocateNetwork respone:\n%+v\n", string(respJson))
